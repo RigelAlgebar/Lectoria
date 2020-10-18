254bc7d58f8735ae7f614fd3f2e730ea457a440d
Miguel Angel <rigel.algebar@gmail.com>
2020-10-18 03:38:09 -0500

    Update utils
    IMportant update regarding access function.

--- src/pages/utils.py
+++ src/pages/utils.py
@@ -3,9 +3,6 @@ from botocore.exceptions import NoCredentialsError
 
 # Document extraction module
 
-ACCESS_KEY = 'AKIAZVR5NA4OUEWG6RVX'
-SECRET_KEY = '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'
-
 def upload_to_aws(file_name, bucket, s3_key):
     s3 = boto3.client('s3', region_name='us-east-2', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
